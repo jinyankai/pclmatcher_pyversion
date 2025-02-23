ab1ff7b95b2a03a19f34ee0838ffef33e9cc2b3f
KIng Kevin <147916477+jinyankai@users.noreply.github.com>
2025-02-23 22:03:05 +0800

    Update mycsf.py

--- mycsf.py
+++ mycsf.py
@@ -15,11 +15,26 @@ def extract_error_data(bag_file, topic='/error'):
 
     return np.array(timestamps), np.array(error_values)
 
+# 对两个rosbag文件的时间戳范围进行截断，确保它们的x轴有相同的数据范围
+def truncate_data(timestamps1, error_values1, timestamps2, error_values2):
+    # 计算两个时间范围的交集
+    start_time = max(timestamps1.min(), timestamps2.min())
+    end_time = min(timestamps1.max(), timestamps2.max())
+
+    # 截取数据，使得两个时间轴在同一范围内
+    mask1 = (timestamps1 >= start_time) & (timestamps1 <= end_time)
+    mask2 = (timestamps2 >= start_time) & (timestamps2 <= end_time)
+
+    return timestamps1[mask1], error_values1[mask1], timestamps2[mask2], error_values2[mask2]
+
 # 绘制两个rosbag文件中的/error话题数据
 def plot_error_data(file1, file2):
     # 提取两个rosbag文件中的/error数据
     timestamps1, error_values1 = extract_error_data(file1)
     timestamps2, error_values2 = extract_error_data(file2)
+
+    # 截断数据以确保时间范围一致
+    timestamps1, error_values1, timestamps2, error_values2 = truncate_data(timestamps1, error_values1, timestamps2, error_values2)
 
     # 创建一个图形
     plt.figure(figsize=(10, 6))
